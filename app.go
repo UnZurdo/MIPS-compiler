7276f036fd4e821829910325d7c213fb1b3e57a8
UnZurdo <albertoynono@gmail.com>
2018-04-24 00:05:39 +0200

    hexaOK

--- app.go
+++ app.go
@@ -167,16 +167,52 @@ func requestHexa(w http.ResponseWriter, r *http.Request) {
 		var hexa_code_array[] string
 		for index, elem := range splited {
 			fmt.Println(index, elem)
-				var bin int64
-				if i, err := strconv.ParseInt(elem, 2, 64); err != nil {
+				if _, err := strconv.ParseInt(elem, 2, 64); err != nil {
 					fmt.Println(err)
 				} else {
-					bin = i
-					fmt.Println("decimal",i)
-					ui := uint64(bin)
-					fmt.Println("uint",ui)
+					size := len(elem)
 
-					hexa_code := "0x"+ strconv.FormatUint(ui, 16)
+					d1, _ := strconv.ParseInt(elem[0:size/8], 2, 64)
+					d2, _ := strconv.ParseInt(elem[size/8:2*size/8], 2, 64)
+					d3, _ := strconv.ParseInt(elem[2*size/8:3*size/8], 2, 64)
+					d4, _ := strconv.ParseInt(elem[3*size/8:4*size/8], 2, 64)
+					d5, _ := strconv.ParseInt(elem[4*size/8:5*size/8], 2, 64)
+					d6, _ := strconv.ParseInt(elem[5*size/8:6*size/8], 2, 64)
+					d7, _ := strconv.ParseInt(elem[6*size/8:7*size/8], 2, 64)
+					d8, _ := strconv.ParseInt(elem[7*size/8:8*size/8], 2, 64)
+
+					fmt.Println(d1,d2,d3,d4,d5,d6,d7,d8)
+
+					bin1 := d1
+					bin2 := d2
+					bin3 := d3
+					bin4 := d4
+					bin5 := d5
+					bin6 := d6
+					bin7 := d7
+					bin8 := d8
+
+					fmt.Println("decimal",d1,d2,d3,d4,d5,d6,d7,d8)
+					ui1 := uint64(bin1)
+					ui2 := uint64(bin2)
+					ui3 := uint64(bin3)
+					ui4 := uint64(bin4)
+					ui5 := uint64(bin5)
+					ui6 := uint64(bin6)
+					ui7 := uint64(bin7)
+					ui8 := uint64(bin8)
+
+					fmt.Println("uint",ui1,ui2,ui3,ui4,ui5,ui6,ui7,ui8)
+
+					hexa_code := "0x"+ strconv.FormatUint(ui1, 16)
+					hexa_code += strconv.FormatUint(ui2, 16)
+					hexa_code += strconv.FormatUint(ui3, 16)
+					hexa_code += strconv.FormatUint(ui4, 16)
+					hexa_code += strconv.FormatUint(ui5, 16)
+					hexa_code += strconv.FormatUint(ui6, 16)
+					hexa_code += strconv.FormatUint(ui7, 16)
+					hexa_code += strconv.FormatUint(ui8, 16)
+
 					hexa_code_array = append(hexa_code_array, hexa_code)
 					fmt.Println("hexa",hexa_code)
 				}
